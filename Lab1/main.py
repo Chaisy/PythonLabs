b2f9c356ab5f35372fcf459256c86567d0c3eeec
Darya <dashulianorton@gmail.com>
2023-02-19 13:00:39 +0300

    added changes to main.py(second point)

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,12 +1,10 @@
-
-
-
+import functions
 
 print("Hello World")
 
+first = int(input("Введите первое целое число: "))
+second = int(input("Введите второе целое число: "))
+opr = input("Введите операцию: ")
 
-
-
-
-
-
+rez, firstFromFunc, secondFromFunc = functions.operation(first, second, opr)
+print("Ответ: ", rez, ".С числами ", firstFromFunc, " и ", secondFromFunc)
