1f5b797398a72cd9db948c4b512f32d0c8d3e851
Darya <dashulianorton@gmail.com>
2023-02-21 23:53:03 +0300

    update functions(do it more pythonic)

--- Lab1/functions.py
+++ Lab1/functions.py
@@ -3,10 +3,10 @@ def operation(x, y, z):
     if (z == 'add'):
         return x+y, x, y
     elif (z == 'div' and x > y):
-        if y==0 : return print("делить на 0 нельзя!!!"), x, y
+        if not y : return print("делить на 0 нельзя!!!"), x, y
         return x/y, x, y
     elif (z == 'div' and x < y):
-        if x == 0: return print("делить на 0 нельзя!!!"), x, y
+        if not x: return print("делить на 0 нельзя!!!"), x, y
         return y / x, x, y
     elif (z == 'mult'):
         return x * y, x, y
@@ -21,7 +21,7 @@ def evenNumbers(list, newList):
     for num in list:
         if int(num) % 2 == 0: newList.append(num)
         else: int(num)+1
-    if len(newList)==0 : return "Нет четных"
+    if not len(newList) : return "Нет четных либо вы не ввели список"
     else : return newList
 
 def getNumber (a):
