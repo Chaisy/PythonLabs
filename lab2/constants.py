9799fdd668b5a837ed97d04f901ba77bd4b5a15f
Darya <dashulianorton@gmail.com>
2023-03-28 23:37:14 +0300

    add file constants for first task

---
+++ lab2/constants.py
@@ -0,0 +1,8 @@
+K=10
+constantes = (
+        r'Mr\.', r'Mrs\.', r'Dr\.', r'St\.',  r'Lt\.', r'sq\.', r'Rep\.', r'B\.A\.', r'Ph\.D\.',
+        r'etc\.', r'exp\.',  r'ex\.', r'e\.g\.', r'vs\.',  r'P\.S\.', r'ft\.', r'kg\.', r'lbs\.', r'in\.', r'sec\.', r'g\.', r'cm\.',
+        r'Jan\.', r'Feb\.', r'Mar\.', r'Apr\.', r'Jn\.', r'Jl\.', r'Aug\.', r'Sep\.', r'Oct\.', r'Now\.', r'Dec\.',
+        r'Mon\.', r'Tue\.', r'Wed\.', r'Th\.', r'Fr\.', r'Sat\.', r'Sn\.', r'p\.m\.')
+
+
