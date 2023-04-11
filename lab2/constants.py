90a5159007b2a6598c9d636862f6b9b6a5985315
Darya <dashulianorton@gmail.com>
2023-04-11 22:52:14 +0300

    upload fixed one const

--- lab2/constants.py
+++ lab2/constants.py
@@ -4,21 +4,20 @@ constantes = (
         r'etc\.', r'exp\.',  r'ex\.', r'e\.g\.', r'vs\.',  r'P\.S\.', r'ft\.', r'kg\.', r'lbs\.', r'in\.', r'sec\.', r'g\.', r'cm\.',
         r'Jan\.', r'Feb\.', r'Mar\.', r'Apr\.', r'Jn\.', r'Jl\.', r'Aug\.', r'Sep\.', r'Oct\.', r'Now\.', r'Dec\.',
         r'Mon\.', r'Tue\.', r'Wed\.', r'Th\.', r'Fr\.', r'Sat\.', r'Sn\.', r'p\.m\.')
+PATH = "/home/dari/PycharmProjects/Lab2IGI/Filiki/"
 
-class commands(Enum):
-
-        ADD = "add"
-        REMOVE = "remove"
-        FIND = "find"
-        GREP = "grep"
-        LIST = "list"
-        LOAD = "load"
-        SWITCH = "switch"
-        EXIT = "exit"
-        PATH = "./Filiki"
-        YES = "y"
-        NO = "n"
-        HELP_COMMANDS = "add *smth* :  add new text in container\n\
+ADD = "add"
+REMOVE = "remove"
+FIND = "find"
+GREP = "grep"
+LIST = "list"
+LOAD = "load"
+SWITCH = "switch"
+EXIT = "exit"
+HELP="help"
+YES = "y"
+NO = "n"
+HELP_COMMANDS = "add *smth* :  add new text in container\n\
 remove *smth* : delete text from container\n\
 find *smth* : if we have element in container we will show it\n\
 list : show all text from container\n\
