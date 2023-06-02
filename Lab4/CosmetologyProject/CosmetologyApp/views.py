0948924c74194453d65699a545713739a7a9301a
Darya <dashulianorton@gmail.com>
2023-06-02 23:06:43 +0300

    add details pages for client, doctor, add shedule

--- Lab4/CosmetologyProject/CosmetologyApp/views.py
+++ Lab4/CosmetologyProject/CosmetologyApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from datetime import datetime
+
+from django.views import generic, View
+
+from .models import Service, Client, Doctor, Shedule
+
 
 def index(request):
     now = datetime.now()
@@ -28,15 +33,51 @@ def about(request):
         }
     )
 
-def service(request):
-    return render(
-        request,
-        "CosmetologyApp/service.html",
-        {
-            'title': "About HelloDjangoApp",
-            'content': "Example app page for Django."
-        }
-    )
+class ServiceListView(generic.ListView):
+    model = Service
+    context_object_name = 'service_list'
+    template_name = 'CosmetologyApp/service.html'
 
-def placements(request):
-    return HttpResponse("PLACEMENTS")
+class ClientListView(generic.ListView):
+    model = Client
+    context_object_name = 'client_list'
+    template_name = 'CosmetologyApp/client.html'
+
+class DoctorListView(generic.ListView):
+    model = Doctor
+    context_object_name = 'doctor_list'
+    template_name = 'CosmetologyApp/doctor.html'
+
+class SheduleListView(generic.ListView):
+    model = Shedule
+    context_object_name = 'shedule_list'
+    template_name = 'CosmetologyApp/shedule.html'
+
+# class ClientDetailView(generic.DetailView):
+#     model = Client
+class ClientDetailView(View):
+    def get(request, id):
+        try:
+            client_details = Client.objects.get(id=id)
+        except Client.DoesNotExist:
+            raise Http404("Placement doesn't exist :(")
+
+        return render(
+            request,
+            'CosmetologyApp/client_detail.html',
+            context={'client_details': client_details, }
+        )
+
+
+class DoctorDetailView(View):
+    def get(request, id):
+        try:
+            doctor_details = Client.objects.get(id=id)
+        except Client.DoesNotExist:
+            raise Http404("Placement doesn't exist :(")
+
+        return render(
+            request,
+            'CosmetologyApp/doctor_detail.html',
+            context={'doctor_details': doctor_details, }
+        )
