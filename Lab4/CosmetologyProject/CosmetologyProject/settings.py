efbf392ff110179fd7227da73dd90f893ad5092a
Darya <dashulianorton@gmail.com>
2023-06-03 19:17:14 +0300

    add line for registration

--- Lab4/CosmetologyProject/CosmetologyProject/settings.py
+++ Lab4/CosmetologyProject/CosmetologyProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
